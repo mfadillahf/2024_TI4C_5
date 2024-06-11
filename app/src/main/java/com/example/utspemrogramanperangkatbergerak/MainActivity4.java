a885e1fa2c425d0c0f2ae615001bce1adf5ce65c
Muhammad Akmal <makmal1709@gmail.com>
2024-06-11 15:58:04 +0800

    first commit

---
+++ app/src/main/java/com/example/utspemrogramanperangkatbergerak/MainActivity4.java
@@ -0,0 +1,19 @@
+package com.example.utspemrogramanperangkatbergerak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity4 extends AppCompatActivity {
+    private Button btkembali;
+    //Deklarasi variabel lainnya
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
